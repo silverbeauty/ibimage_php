af69acab8c8bfbeceaea5cb928da28fa49338747
Maksim Platov <32350089+superdev2017@users.noreply.github.com>
2018-04-23 23:50:05 +1000

    Update database
    Update database

--- config/app.php
+++ config/app.php
@@ -252,7 +252,7 @@ return [
              */
             //'port' => 'non_standard_port_number',
             'username' => 'root',
-            'password' => '',
+            'password' => 'Ianben123',
             'database' => 'ibimagesinfo',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
